98c5447007fad07d2e94fccd950ffe20266de93e
Charlie Tanksley <charlie.tanksley@gmail.com>
2011-09-14 16:41:01 -0400

    Use nested output for development.

--- config.rb
+++ config.rb
@@ -10,7 +10,8 @@ images_dir = "public/images"
 javascripts_dir = "public/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
-output_style = :compressed
+#output_style = :compressed
+output_style = :nested
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
