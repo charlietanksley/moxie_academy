429301468fa1f4df8867fc618a199f5eb74961ce
Charlie Tanksley <charlie.tanksley@gmail.com>
2011-09-16 15:34:22 -0400

    First pass at Lessons controller (works enough).

--- app/controllers/lessons.rb
+++ app/controllers/lessons.rb
@@ -20,24 +20,58 @@ MoxieAcademy.controllers :lessons do
 
   # INDEX
   get :index do
-  end
-
-  # INDIVIDUAL
-  get :show, :with => :slug do
+    @lessons = Lesson.all
+    render 'lessons/index'
   end
 
   # NEW
   get :new do
+    render 'lessons/new'
   end
 
   post :new do
+    lesson = Lesson.new(params[:lesson])
+    if lesson.save!
+      @lesson = lesson
+      redirect ("/lessons/#{@lesson.slug}")
+    else
+      flash[:error] = "Sorry, something went wrong!"
+      redirect to('/lessons/new-lesson')
+    end
   end
 
   # EDIT
   get :edit do
+    @lesson = Lesson.first(:id => params[:id])
+    render 'lessons/edit'
   end
 
   post :edit do
+    lesson_info = remove_empty_fields(params[:lesson])
+    lesson = Lesson.first(:id => lesson_info['id'])
+    if lesson.update lesson_info
+      flash[:notice] = 'Success!'
+      redirect to('/admin/lessons')
+    else
+      flash[:error] = 'Something went wrong :('
+    end
+  end
+
+  get :destroy do
+    lesson = Lesson.first(:id => params[:id])
+    flash[:notice] = "The lesson '#{lesson.title}' has been removed"
+    lesson.destroy
+    redirect to('/admin/lessons')
   end
   
+  # INDIVIDUAL (has to be last or it will gobble up all lesson paths!)
+  get :show, :map => 'lessons/:slug' do
+    begin
+      @lesson = Lesson.first(:slug => params[:slug])
+      render 'lessons/show'
+    rescue
+      lesson_not_found
+    end
+  end
+
 end
