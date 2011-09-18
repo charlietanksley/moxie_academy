2834f1218ca2a49554d15b80b7db48c1c9af9778
Charlie Tanksley <charlie.tanksley@gmail.com>
2011-09-18 16:07:16 -0400

    Get Admin users up and running
    Same story as last time: need tests and validations!

--- admin/controllers/users.rb
+++ admin/controllers/users.rb
@@ -1,29 +1,26 @@
 Admin.controllers :users do
 
-  # INDEX {{{
-
   get :index do
     @users = User.all
-    @groups = Group.all
-    render 'admin/users'
+    render 'users/index'
   end
 
-  # INDEX }}}
   # NEW {{{
 
   get :new do
+    @groups = Group.all
     render 'users/new'
   end
 
   post :create do
-    credentials = params[:user]
-    user = User.new(credentials)
-    if user.save
-      flash[:notice] = 'Success!'
-      redirect to('/admin/users')
+    user = User.new(params[:user])
+    if user.save!
+      flash[:notice] = 'User created.'
+      redirect url(:users, :index)
     else
-      flash[:error] = 'Something went wrong :('
-      redirect to('/admin/users')
+      flash[:error] = "Sorry, something went wrong!"
+      #redirect url(:users, :index)
+      #redirect to('/users/')
     end
   end
 
@@ -31,37 +28,29 @@ Admin.controllers :users do
   # EDIT {{{
 
   get :edit, :with => :id do
-    @user = User.first(:id => params[:id])
     @groups = Group.all
-    render 'admin/edit-user'
+    @user = User.first(params[:id])
+    render 'users/edit'
   end
 
-  post :update, :with => :id do
-    credentials = remove_empty_fields(params[:user])
-    user = User.first(:id => credentials['id'])
-    if user.update credentials
-      flash[:notice] = 'Success!'
-      redirect to('/admin/users')
+  put :update, :with => :id do
+    @user = User.first(params[:id])
+    if @user.update(params[:user])
+      flash[:notice] = 'User was successfully updated.'
+      redirect url(:users, :edit, :id => @user.id)
     else
-      flash[:error] = 'Something went wrong :('
-      redirect to('/admin/users')
+      render 'users/edit'
     end
   end
-  
+
   # END EDIT }}}
   # DESTROY {{{
 
-  delete :destroy, :with => :id do
+  delete :destroy do
     user = User.first(:id => params[:id])
-    #if account != current_account && account.destroy
-    #  flash[:notice] = 'Account was successfully destroyed.'
-    #else
-    #  flash[:error] = 'Unable to destroy Account!'
-    #end
-    #redirect url(:accounts, :index)
-    flash[:notice] = "The account for #{user.email} has been removed"
+    flash[:notice] = "The user '#{user.email}' has been removed"
     user.destroy
-    redirect to('/admin/users')
+    redirect url(:users, :index)
   end
 
   # END DESTROY }}}
