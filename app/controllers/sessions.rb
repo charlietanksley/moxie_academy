007eaa4dc0d9b7ef5756154d330e71ba8ae3bc52
Charlie Tanksley <charlie.tanksley@gmail.com>
2011-09-18 14:02:15 -0400

    Fix login and logout mappings.

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -1,11 +1,12 @@
-MoxieAcademy.controllers :users do
+#MoxieAcademy.controllers :users do
+MoxieAcademy.controllers :sessions do
 
   # get :index, :map => "/foo/bar" do
   get :new, :map => '/login' do
     render 'sessions/new'
   end
 
-  post :create do
+  post :create, :map => '/login' do
     credentials = params[:login]
     user = User.first(:email => credentials[:email])
           
@@ -20,6 +21,14 @@ MoxieAcademy.controllers :users do
       flash[:error] = 'There seems to have been a problem.  Please enter your email and password again.'
       redirect url(:sessions, :new)
     end
+  end
+
+  get '/logout' do
+    #redirect url(:sessions, :destroy, :method => :delete)
+    session[:logged_in_as] = nil
+    session[:logged_in] = nil
+
+    redirect url(:sessions, :new)
   end
 
   delete :destroy do
