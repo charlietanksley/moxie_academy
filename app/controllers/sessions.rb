6a26c1e98775e9ffeac95e1ee6c14f5b68371738
Charlie Tanksley <charlie.tanksley@gmail.com>
2011-09-21 13:49:04 -0400

    Set up basic sign in in sessions controller

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -7,48 +7,35 @@ MoxieAcademy.controllers :sessions do
   end
 
   post :create do
-    if account = Account.authenticate(params[:email], params[:password])
-      set_current_account(account)
-      redirect url(:base, :index)
-    elsif Padrino.env == :development && params[:bypass]
-      account = Account.first
-      set_current_account(account)
-      redirect url(:base, :index)
+    user = User.first(:email => params[:login][:email]) # Deal with wrong email
+    if user.nil?
+      flash[:error] = 'You must have entered your email address incorrectly.'
+      redirect url(:sessions, :new)
+    end
+
+    if params[:login][:password].nil?
+      flash[:error] = 'You must enter a password.'
+      redirect url(:sessions, :new)
+    end
+
+    if params[:login][:password] == Group.first(:id => user.group_id).password
+      session[:uid] = user.id
+      redirect session[:back] if session[:back]
+      redirect url(:lessons, :index)
     else
-      flash[:warning] = "Login or password wrong."
+      flash[:error] = 'You must have entered your password address incorrectly.'
       redirect url(:sessions, :new)
     end
   end
-  #post :create, :map => '/login' do
-  #  credentials = params[:login]
-  #  user = User.first(:email => credentials[:email])
-  #        
-  #  if user.group.password == credentials[:password]
-  #    session[:logged_in_as] = user.group_id
-  #    session[:logged_in] = true
-  #    "#{session}: why you no back?"
-  #    #if session[:back]
-  #    #  redirect session[:back]
-  #    #else
-  #    #  redirect url(:lessons, :index)
-  #    #end
-  #  else
-  #    flash[:error] = 'There seems to have been a problem.  Please enter your email and password again.'
-  #    redirect url(:sessions, :new)
-  #  end
-  #end
 
   get '/logout' do
-    #redirect url(:sessions, :destroy, :method => :delete)
-    session[:logged_in_as] = nil
-    session[:logged_in] = nil
+    session[:uid] = nil
 
     redirect url(:sessions, :new)
   end
 
   delete :destroy do
-    session[:logged_in_as] = nil
-    session[:logged_in] = nil
+    session[:uid] = nil
 
     redirect url(:sessions, :new)
   end
