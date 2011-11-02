6b754ed7e8a71341e1674e0374a82a7c8a7c270d
Charlie Tanksley <charlie.tanksley@gmail.com>
2011-11-02 17:08:45 -0400

    Clean up Discussion step definitions a bit

--- features/step_definitions/discussion.rb
+++ features/step_definitions/discussion.rb
@@ -1,12 +1,12 @@
 Given /^A user exists$/ do
-  Group.create(:name => 'sample',
-               :password => 'password')
-  User.create(:email => 'joe@email.com',
-              :group_id => Group.first(:name => 'sample').id)
+  @group = Group.create(:name => 'sample',
+                        :password => 'password')
+  @user = User.create(:email => 'joe@email.com',
+                      :group_id => Group.first(:name => 'sample').id)
 end
 
 Given /^A discussion exists$/ do
-  Discussion.create(:group_id => Group.first(:name => 'sample').id)
+  @discussion = Discussion.create(:group_id => @group.id)#Group.first(:name => 'sample').id)
 end
   
 Given /^I am logged in as a user$/ do
@@ -20,20 +20,18 @@ end
 
 Given /^There are some comments$/ do
   Given 'A discussion exists'
-
-  discussion = Discussion.first(:group_id => Group.first(:name => 'sample').id)
   joe = User.first(:email => 'joe@email.com')
 
-  Comment.create(:author => joe,
-                 :discussion_id => discussion.id,
-                 :body => 'OMG, I love it!')
-  Comment.create(:author => joe,
-                 :discussion_id => discussion.id,
-                 :body => 'ZOMG, I double love it!')
+  @comment1 = Comment.create(:user_id => joe,
+                             :discussion_id => @discussion.id,
+                             :body => 'OMG, I love it!')
+  @comment2 = Comment.create(:user_id => joe,
+                             :discussion_id => @discussion.id,
+                             :body => 'ZOMG, I double love it!')
 end
 
 When /^I look at the discussions$/ do
-  pending # express the regexp above with the code you wish you had
+  visit Conversation.url(:discussion, :index, :id => @group.id)
 end
 
 Then /^I see the comments$/ do
