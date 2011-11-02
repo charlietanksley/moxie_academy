ce924746214bc378366506dea211ac18be77ebff
Charlie Tanksley <charlie.tanksley@gmail.com>
2011-11-02 13:59:33 -0400

    Add validation of group_id for Discussions

--- models/discussion.rb
+++ models/discussion.rb
@@ -4,4 +4,6 @@ class Discussion
   # property <name>, <type>
   property :id, Serial
   property :group_id, Integer
+
+  validates_presence_of :group_id
 end
--- spec/models/discussion_spec.rb
+++ spec/models/discussion_spec.rb
@@ -1,8 +1,19 @@
 require 'spec_helper'
 
 describe "Discussion Model" do
+  let(:group) { create(:group) }
   let(:discussion) { Discussion.new }
+  let(:complete_discussion) { Discussion.new(:group_id => group.id) }
+
   it 'can be created' do
     discussion.should_not be_nil
+  end
+
+  it 'is invalid without group_id' do
+    discussion.should_not be_valid
+  end
+
+  it 'is valid if it has a group_id' do
+    complete_discussion.should be_valid
   end
 end
