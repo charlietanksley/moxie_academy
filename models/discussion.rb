1082de04eee225816893e82eb86079fae0057b25
Charlie Tanksley <charlie.tanksley@gmail.com>
2011-11-06 08:51:17 -0500

    Add Discussion model

--- models/discussion.rb
+++ models/discussion.rb
@@ -10,6 +10,6 @@ class Discussion
   has n, :comments
 
   def self.discussion_for(group_id)
-    first(:group_id => group_id).comments
+    first_or_create(:group_id => group_id).comments(:order => :id.desc)
   end
 end
